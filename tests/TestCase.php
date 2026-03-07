75cd8d871db0314dd655270ec9d14561b6239378
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-03-07 21:05:11 +0000

    Lint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,7 +14,6 @@ use Modules\Xot\Tests\XotBaseTestCase;
  */
 abstract class TestCase extends XotBaseTestCase
 {
-
     /** @var array<int, string> */
     protected array $connectionsToTransact = [
         'mysql',

ec649971c475f4be8d858f8b9441f162595704b9
marco76tv <marco.sottana@gmail.com>
2026-03-09 15:53:57 +0100

    .

--- tests/Feature/RegisterLocalizationTest.php
+++ tests/Feature/RegisterLocalizationTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Modules\Gdpr\Tests\Feature;
+
 /**
  * Localization Tests for Registration Page.
  *
--- tests/Feature/RegisterPageComprehensiveTest.php
+++ tests/Feature/RegisterPageComprehensiveTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Modules\Gdpr\Tests\Feature;
+
 /**
  * Comprehensive Registration Page Tests.
  *

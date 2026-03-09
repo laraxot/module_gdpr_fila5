fdf42cf12721c5212d932240764bc13a25045e48
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-03-09 14:54:13 +0000

    Lint

--- tests/Feature/RegisterLocalizationTest.php
+++ tests/Feature/RegisterLocalizationTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Tests\Feature;
 
-/**
+/*
  * Localization Tests for Registration Page.
  *
  * Tests that all supported locales have complete translations:

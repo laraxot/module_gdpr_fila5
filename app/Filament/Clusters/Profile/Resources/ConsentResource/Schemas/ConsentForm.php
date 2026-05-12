09895113d3ac50fbc485d33d1e0f07717070777d
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:10:33 +0200

    .

--- app/Filament/Clusters/Profile/Resources/ConsentResource/Schemas/ConsentForm.php
+++ app/Filament/Clusters/Profile/Resources/ConsentResource/Schemas/ConsentForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Gdpr\app\Filament\Clusters\Profile\Resources\ConsentResource\Schemas;
+namespace Modules\Gdpr\Filament\Clusters\Profile\Resources\ConsentResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
@@ -11,7 +11,7 @@ use Modules\Xot\Filament\Resources\Schemas\XotBaseResourceForm;
 class ConsentForm extends XotBaseResourceForm
 {
     /**
-     * @return array<int|string, \Filament\Schemas\Components\Component>
+     * @return array<int|string, \Filament\Forms\Components\Component>
      */
     public static function getFormSchema(): array
     {

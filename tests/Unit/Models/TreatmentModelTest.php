b76b09b90989e075bdb85a51ffdb925c2cf25892
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-05-12 13:07:35 +0000

    Lint

--- tests/Unit/Models/TreatmentModelTest.php
+++ tests/Unit/Models/TreatmentModelTest.php
@@ -10,7 +10,7 @@ use Modules\Gdpr\Models\Treatment;
 use Modules\Gdpr\Tests\TestCase;
 
 test('treatment_fillable_attributes', function () {
-    $treatment = new Treatment;
+    $treatment = new Treatment();
     $fillable = $treatment->getFillable();
 
     expect($fillable)->toContain('id');
@@ -24,20 +24,20 @@ test('treatment_fillable_attributes', function () {
 });
 
 test('treatment_is_not_incrementing', function () {
-    $treatment = new Treatment;
+    $treatment = new Treatment();
 
     expect($treatment->getIncrementing())->toBeFalse();
 });
 
 test('treatment_is_uuid', function () {
-    $treatment = new Treatment;
+    $treatment = new Treatment();
     $traits = class_uses_recursive($treatment);
 
     expect($traits)->toHaveKey('Illuminate\Database\Eloquent\Concerns\HasUuids');
 });
 
 test('treatment_extends_base_model', function () {
-    $treatment = new Treatment;
+    $treatment = new Treatment();
 
     expect($treatment)->toBeInstanceOf(Modules\Gdpr\Models\BaseModel::class);
 });

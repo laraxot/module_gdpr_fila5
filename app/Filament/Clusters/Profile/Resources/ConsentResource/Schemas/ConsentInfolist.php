fe2471f3609d19808e22dfa94b4429483a3018a8
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:10:33 +0200

    .

--- app/Filament/Clusters/Profile/Resources/ConsentResource/Schemas/ConsentInfolist.php
+++ app/Filament/Clusters/Profile/Resources/ConsentResource/Schemas/ConsentInfolist.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Gdpr\app\Filament\Clusters\Profile\Resources\ConsentResource\Schemas;
+namespace Modules\Gdpr\Filament\Clusters\Profile\Resources\ConsentResource\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
 

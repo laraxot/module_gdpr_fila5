7ac73d7956a8d83ef5ff60d579f9a71f9912d7e9
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-05-13 15:18:46 +0000

    Lint

--- app/Actions/Validation/ValidateUserDataAction.php
+++ app/Actions/Validation/ValidateUserDataAction.php
@@ -19,7 +19,6 @@ class ValidateUserDataAction
      * @param array<string, mixed> $formData
      * @param array<string, mixed> $formData
      *
-     * @param  array<string, mixed>  $formData
      * @return array<string, mixed>
      */
     public function execute(array $formData): array

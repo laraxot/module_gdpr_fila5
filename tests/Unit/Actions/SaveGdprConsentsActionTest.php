4cbcdbca7c0a0caf6777c46bcb64f9055b11777b
marco76tv <marco.sottana@gmail.com>
2026-05-20 07:59:32 +0200

    .

--- tests/Unit/Actions/SaveGdprConsentsActionTest.php
+++ tests/Unit/Actions/SaveGdprConsentsActionTest.php
@@ -10,13 +10,13 @@ use Modules\Gdpr\Actions\SaveGdprConsentsAction;
 use Modules\Gdpr\Tests\TestCase;
 
 test('SaveGdprConsentsAction can be instantiated', function () {
-    $action = new SaveGdprConsentsAction();
-    $action = new SaveGdprConsentsAction();
+    $action = new SaveGdprConsentsAction;
+    $action = new SaveGdprConsentsAction;
     expect($action)->toBeInstanceOf(SaveGdprConsentsAction::class);
 });
 
 test('SaveGdprConsentsAction execute method exists', function () {
-    $action = new SaveGdprConsentsAction();
-    $action = new SaveGdprConsentsAction();
+    $action = new SaveGdprConsentsAction;
+    $action = new SaveGdprConsentsAction;
     expect(method_exists($action, 'execute'))->toBeTrue();
 });

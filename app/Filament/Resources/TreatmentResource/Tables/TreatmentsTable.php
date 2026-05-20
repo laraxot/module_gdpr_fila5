8c7e831442dbad500801c468e80acfb8777399fd
marco76tv <marco.sottana@gmail.com>
2026-05-20 07:59:27 +0200

    .

--- app/Filament/Resources/TreatmentResource/Tables/TreatmentsTable.php
+++ app/Filament/Resources/TreatmentResource/Tables/TreatmentsTable.php
@@ -2,31 +2,23 @@
 
 declare(strict_types=1);
 
-namespace Modules\Gdpr\Filament\Resources\TreatmentResource\Tables;
+namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Gdpr\app\Filament\Resources\TreatmentResource\Tables;
 
-use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
 class TreatmentsTable extends XotBaseResourceTable
 {
+    /**
+     * @return array<string, Column>
+     */
     public static function getTableColumns(): array
     {
         return [
-            'active' => IconColumn::make('active')->boolean(),
-            'required' => IconColumn::make('required')->boolean(),
+            'id' => TextColumn::make('id')->sortable(),
             'name' => TextColumn::make('name')->searchable(),
-            'documentVersion' => TextColumn::make('documentVersion')->searchable(),
-            'documentUrl' => TextColumn::make('documentUrl')->searchable(),
-            'weight' => TextColumn::make('weight')->numeric()->sortable(),
-            'created_at' => TextColumn::make('created_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
-            'updated_at' => TextColumn::make('updated_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
+            'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
         ];
     }
 }

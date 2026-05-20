cb124d0853bf4db82a8488f9cca4f9440f1534e6
marco76tv <marco.sottana@gmail.com>
2026-05-20 07:59:32 +0200

    .

--- tests/Unit/Actions/HandleRegistrationErrorActionTest.php
+++ tests/Unit/Actions/HandleRegistrationErrorActionTest.php
@@ -10,13 +10,13 @@ use Modules\Gdpr\Actions\Registration\HandleRegistrationErrorAction;
 use Modules\Gdpr\Tests\TestCase;
 
 test('HandleRegistrationErrorAction can be instantiated', function () {
-    $action = new HandleRegistrationErrorAction();
-    $action = new HandleRegistrationErrorAction();
+    $action = new HandleRegistrationErrorAction;
+    $action = new HandleRegistrationErrorAction;
     expect($action)->toBeInstanceOf(HandleRegistrationErrorAction::class);
 });
 
 test('HandleRegistrationErrorAction execute method exists', function () {
-    $action = new HandleRegistrationErrorAction();
-    $action = new HandleRegistrationErrorAction();
+    $action = new HandleRegistrationErrorAction;
+    $action = new HandleRegistrationErrorAction;
     expect(method_exists($action, 'execute'))->toBeTrue();
 });

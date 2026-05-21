b348467163cf3b29c15d7e79643d0be68d19f7cf
marco76tv <marco.sottana@gmail.com>
2026-05-21 10:46:53 +0200

    .

--- app/Filament/Resources/ConsentResource/Tables/ConsentsTable.php
+++ app/Filament/Resources/ConsentResource/Tables/ConsentsTable.php
@@ -11,18 +11,13 @@ class ConsentsTable extends XotBaseResourceTable
 {
     public static function getTableColumns(): array
     {
+        /*
+         * @return array<int|string, \Filament\Tables\Columns\Column>
+         */
         return [
-            'id' => TextColumn::make('id')->searchable(),
-            'treatment' => TextColumn::make('treatment.name')->searchable(),
-            'subject_id' => TextColumn::make('subject_id')->searchable(),
-            'created_at' => TextColumn::make('created_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
-            'updated_at' => TextColumn::make('updated_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
+            'id' => TextColumn::make('id')->searchable()->sortable(),
+            'created_at' => TextColumn::make('created_at')->dateTime(),
+            'updated_at' => TextColumn::make('updated_at')->dateTime(),
         ];
     }
 }

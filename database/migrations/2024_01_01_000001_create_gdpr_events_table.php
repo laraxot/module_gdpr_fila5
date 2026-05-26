f9ef6e7a31dc2463c68d59622b4fe6fea416baef
Malebestia <malebestia@yahoo.it>
2026-05-26 11:47:41 +0200

    .

--- database/migrations/2024_01_01_000001_create_gdpr_events_table.php
+++ database/migrations/2024_01_01_000001_create_gdpr_events_table.php
@@ -16,7 +16,7 @@ return new class extends XotBaseMigration {
     {
         // -- CREATE --
 
-        // @var mixed tableCreate(function (Blueprint $table
+        $this->tableCreate(function (Blueprint $table
             $table->uuid('id')->primary();
             $table->uuid('treatment_id')->nullable();
             $table->uuid('consent_id')->nullable();
@@ -43,11 +43,11 @@ return new class extends XotBaseMigration {
         });
 
         // -- UPDATE --
-        // @var mixed tableUpdate(function (Blueprint $table
-            // if (! // @var mixed hasColumn('email'
+        $this->tableUpdate(function (Blueprint $table
+            // if (! $this->hasColumn('email'
             //    $table->string('email')->nullable();
             // }
-            // @var mixed updateTimestamps(
+            $this->updateTimestamps(
                 table: $table,
                 hasSoftDeletes: true,
             );

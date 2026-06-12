ae412f08d0d7150b8a586900d54f66f9c2efd8a6
marco76tv <marco.sottana@gmail.com>
2026-06-12 13:45:04 +0200

    .

--- database/factories/TreatmentFactory.php
+++ database/factories/TreatmentFactory.php
@@ -12,16 +12,19 @@ use Modules\Gdpr\Models\Treatment;
  */
 class TreatmentFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     */
     protected $model = Treatment::class;
 
     /**
-     * Define the model's default state.
+     * @return array<string, mixed>
      */
     public function definition(): array
     {
-        return [];
+        return [
+            'name' => 'treatment-'.fake()->unique()->uuid(),
+            'description' => fake()->sentence(),
+            'weight' => fake()->numberBetween(1, 10),
+            'active' => true,
+            'required' => false,
+        ];
     }
 }

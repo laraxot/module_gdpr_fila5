4d061271c3eebc9531a0bbc0f43b1bc75a3b110b
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-06-17 14:19:44 +0000

    Lint

--- tests/Unit/Actions/SaveGdprConsentsActionTest.php
+++ tests/Unit/Actions/SaveGdprConsentsActionTest.php
@@ -8,7 +8,7 @@ use Modules\Gdpr\Actions\SaveGdprConsentsAction;
 use Modules\Gdpr\Tests\TestCase;
 use PHPUnit\Framework\Assert;
 
-uses(\Modules\Gdpr\Tests\TestCase::class);
+uses(TestCase::class);
 
 test('SaveGdprConsentsAction can be instantiated', function (): void {
     $action = new SaveGdprConsentsAction();

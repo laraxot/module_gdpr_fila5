13657daa4e734e642aca982d048adbf87157f7d8
marco76tv <marco.sottana@gmail.com>
2026-07-06 20:42:20 +0200

    .

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -148,7 +148,6 @@ use function Safe\json_encode;
  * @property Consent|null         $consent
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
- *
  * @method static Builder<static>|Event newModelQuery()
  * @method static Builder<static>|Event newQuery()
  * @method static Builder<static>|Event query()
@@ -165,11 +164,8 @@ use function Safe\json_encode;
  * @method static Builder<static>|Event whereTreatmentId($value)
  * @method static Builder<static>|Event whereUpdatedAt($value)
  * @method static Builder<static>|Event whereUpdatedBy($value)
- *
  * @property ProfileContract|null $deleter
- *
  * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
- *
  * @mixin \Eloquent
  */
 class Event extends BaseModel
@@ -207,11 +203,11 @@ class Event extends BaseModel
 
     public function setPayloadAttribute(?string $value): void
     {
-        $attributes['payload'] = Crypt::encrypt(json_encode($value, JSON_THROW_ON_ERROR));
+        $this->attributes['payload'] = Crypt::encrypt(json_encode($value, JSON_THROW_ON_ERROR));
     }
 
     public function setIpAttribute(?string $value): void
     {
-        $attributes['ip'] = Crypt::encrypt($value);
+        $this->attributes['ip'] = Crypt::encrypt($value);
     }
 }

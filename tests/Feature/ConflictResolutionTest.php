e4024fa49eb8799fd06946b946a1c88aef63a109
marco76tv <marco.sottana@gmail.com>
2026-07-06 20:41:08 +0200

    .

--- tests/Feature/ConflictResolutionTest.php
+++ tests/Feature/ConflictResolutionTest.php
@@ -10,13 +10,18 @@ use PHPUnit\Framework\Assert;
 uses(TestCase::class);
 
 it('verifica che le classi corrette siano istanziabili', function (): void {
+<<<<<<< HEAD
     Assert::assertInstanceOf(Treatment::class, new Treatment());
     Assert::assertInstanceOf(Profile::class, new Profile());
+=======
+    expect(new Treatment)->toBeInstanceOf(Treatment::class);
+    expect(new Profile)->toBeInstanceOf(Profile::class);
+>>>>>>> 2ae5567 (.)
 });
 
 it('verifica che le proprietà delle classi siano accessibili', function (): void {
-    $treatment = new Treatment();
-    $profile = new Profile();
+    $treatment = new Treatment;
+    $profile = new Profile;
 
     Assert::assertIsArray($treatment->getFillable());
     Assert::assertIsArray($profile->getFillable());

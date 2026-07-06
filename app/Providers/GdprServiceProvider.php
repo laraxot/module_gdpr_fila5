5a54c68d1b3a0111b779e3a7c001f37e65fb15ca
marco76tv <marco.sottana@gmail.com>
2026-07-06 20:44:46 +0200

    .

--- app/Providers/GdprServiceProvider.php
+++ app/Providers/GdprServiceProvider.php
@@ -7,8 +7,9 @@ namespace Modules\Gdpr\Providers;
 use Illuminate\Routing\Router;
 use Modules\Gdpr\Datas\GdprData;
 use Modules\Xot\Providers\XotBaseServiceProvider;
-use function Safe\realpath;
 use Statikbe\CookieConsent\CookieConsentMiddleware;
+
+use function Safe\realpath;
 
 class GdprServiceProvider extends XotBaseServiceProvider
 {

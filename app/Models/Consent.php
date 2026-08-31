71b2e09678268e544dc4bee7ce5a77e86e8a63c4
Marco Xot <marco76tv@hotmail.com>
2026-08-31 21:27:18 +0200

    .

--- app/Models/Consent.php
+++ app/Models/Consent.php
@@ -45,7 +45,6 @@ use Modules\Xot\Datas\XotData;
  * @property ProfileContract|null $creator
  * @property Treatment|null       $treatment
  * @property ProfileContract|null $updater
- *
  * @method static Builder<static>|Consent newModelQuery()
  * @method static Builder<static>|Consent newQuery()
  * @method static Builder<static>|Consent query()
@@ -62,22 +61,17 @@ use Modules\Xot\Datas\XotData;
  * @method static Builder<static>|Consent whereUpdatedBy($value)
  * @method static Builder<static>|Consent whereUserId($value)
  * @method static Builder<static>|Consent whereUserType($value)
- *
  * @property ProfileContract|null $deleter
  * @property string|null          $ip_address
  * @property string|null          $user_agent
- *
  * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
  * @method static Builder<static>|Consent                         whereIpAddress($value)
  * @method static Builder<static>|Consent                         whereUserAgent($value)
- *
  * @property string|null $ip_address
  * @property string|null $user_agent
- *
  * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
  * @method static Builder<static>|Consent                         whereIpAddress($value)
  * @method static Builder<static>|Consent                         whereUserAgent($value)
- *
  * @mixin \Eloquent
  */
 class Consent extends BaseModel

91782cd84558fcbef3cf2957f5cc4b132ed9df4c
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-09-02 20:27:33 +0000

    Lint

--- tests/Unit/Actions/CollectGdprConsentsActionTest.php
+++ tests/Unit/Actions/CollectGdprConsentsActionTest.php
@@ -8,7 +8,7 @@ use Modules\Gdpr\Actions\Consent\CollectGdprConsentsAction;
 use Modules\Gdpr\Tests\TestCase;
 use PHPUnit\Framework\Assert;
 
-uses(\Modules\Gdpr\Tests\TestCase::class);
+uses(TestCase::class);
 
 test('CollectGdprConsentsAction returns correct array', function () {
     $action = new CollectGdprConsentsAction();

662c40b68034cfcf2efa7de66a623986ed5c42a5
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-09-03 16:02:05 +0000

    Lint

--- tests/PestHelpers.php
+++ tests/PestHelpers.php
@@ -33,7 +33,8 @@ function gdprTest(): TestCase
 }
 
 /**
- * @param  array<string, string>  $headers
+ * @param array<string, string> $headers
+ *
  * @return TestResponse<Response>
  */
 function gdprGet(string $uri, array $headers = []): TestResponse
@@ -42,8 +43,9 @@ function gdprGet(string $uri, array $headers = []): TestResponse
 }
 
 /**
- * @param  array<string, mixed>  $data
- * @param  array<string, string>  $headers
+ * @param array<string, mixed>  $data
+ * @param array<string, string> $headers
+ *
  * @return TestResponse<Response>
  */
 function gdprPost(string $uri, array $data = [], array $headers = []): TestResponse
@@ -57,7 +59,7 @@ function gdprActingAs(Authenticatable $user, ?string $driver = null): TestCase
 }
 
 /**
- * @param  array<string, mixed>  $parameters
+ * @param array<string, mixed> $parameters
  */
 function gdprArtisan(string $command, array $parameters = []): int
 {
@@ -70,7 +72,7 @@ function gdprSkipTest(string $message = ''): void
 }
 
 /**
- * @param  array<string, mixed>  $where
+ * @param array<string, mixed> $where
  */
 function assertGdprTableHas(string $table, array $where, ?string $connection = 'gdpr'): void
 {
@@ -84,7 +86,7 @@ function assertGdprTableHas(string $table, array $where, ?string $connection = '
 }
 
 /**
- * @param  array<string, mixed>  $where
+ * @param array<string, mixed> $where
  */
 function assertGdprTableMissing(string $table, array $where, ?string $connection = 'gdpr'): void
 {
@@ -98,7 +100,7 @@ function assertGdprTableMissing(string $table, array $where, ?string $connection
 }
 
 /**
- * @param  array<string, mixed>  $attributes
+ * @param array<string, mixed> $attributes
  */
 function createGdprConsent(array $attributes = []): Consent
 {
@@ -115,7 +117,7 @@ function gdprAssertDatabaseAvailable(): void
 }
 
 /**
- * @param  class-string<Throwable>  $exceptionClass
+ * @param class-string<Throwable> $exceptionClass
  */
 function gdprAssertThrows(string $exceptionClass, callable $callback): void
 {
@@ -128,7 +130,7 @@ function gdprAssertThrows(string $exceptionClass, callable $callback): void
 }
 
 /**
- * @param  class-string<Throwable>  $exceptionClass
+ * @param class-string<Throwable> $exceptionClass
  */
 function gdprAssertDoesNotThrow(string $exceptionClass, callable $callback): void
 {
@@ -144,8 +146,8 @@ function gdprAssertDoesNotThrow(string $exceptionClass, callable $callback): voi
 }
 
 /**
- * @param  list<string>  $fields
- * @param  array<string>  $fillable
+ * @param list<string>  $fields
+ * @param array<string> $fillable
  */
 function assertFillableContains(array $fields, array $fillable): void
 {

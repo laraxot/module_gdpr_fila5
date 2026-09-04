813fed0fefc41c6b8096fc507b43ff5ae4cfae49
Marco Xot <marco76tv@hotmail.com>
2026-09-04 21:59:32 +0200

    .

--- app/Actions/Validation/ValidateUserDataAction.php
+++ app/Actions/Validation/ValidateUserDataAction.php
@@ -6,9 +6,9 @@ namespace Modules\Gdpr\Actions\Validation;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
-use Modules\User\Models\User;
-use Modules\Xot\Contracts\UserContract;
 use Modules\Xot\Actions\Cast\SafeStringCastAction;
+use Modules\Xot\Contracts\UserContract;
+use Modules\Xot\Datas\XotData;
 use Spatie\QueueableAction\QueueableAction;
 
 class ValidateUserDataAction
@@ -16,17 +16,17 @@ class ValidateUserDataAction
     use QueueableAction;
 
     /**
-     * @param array<string, mixed> $formData
-     * @param array<string, mixed> $formData
-     * @param array<string, mixed> $formData
-     *
+     * @param  array<string, mixed>  $formData
      * @return array<string, mixed>
      */
     public function execute(array $formData): array
     {
         $email = app(SafeStringCastAction::class)->execute($formData['email']);
 
-        if (User::where('email', $email)->exists()) {
+        /** @var class-string<UserContract> $userClass */
+        $userClass = XotData::make()->getUserClass();
+
+        if ($userClass::query()->where('email', $email)->exists()) {
             throw ValidationException::withMessages(['email' => [__('validation.unique', ['attribute' => 'email'])]]);
         }
 

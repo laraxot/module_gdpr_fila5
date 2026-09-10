b65949f5ae7cf3549c5a84038be65959244eab9f
Marco Xot <marco76tv@hotmail.com>
2026-09-10 19:00:11 +0200

    .

--- app/Filament/Resources/ProfileResource/Pages/ListProfiles.php
+++ app/Filament/Resources/ProfileResource/Pages/ListProfiles.php
@@ -13,37 +13,4 @@ use Modules\User\Filament\Resources\BaseProfileResource\Pages\ListProfiles as Us
 class ListProfiles extends UserListProfiles
 {
     protected static string $resource = ProfileResource::class;
-
-    /**
-     * @return array<string, Tables\Columns\Column>
-     */
-    #[\Override]
-    public function getTableColumns(): array
-    {
-        return [
-            'id' => TextColumn::make('id')->searchable(),
-            'type' => TextColumn::make('type')->searchable(),
-            'first_name' => TextColumn::make('first_name')->searchable(),
-            'last_name' => TextColumn::make('last_name')->searchable(),
-            'full_name' => TextColumn::make('full_name')->searchable(),
-            'email' => TextColumn::make('email')->searchable(),
-            'created_at' => TextColumn::make('created_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
-            'updated_at' => TextColumn::make('updated_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
-            'user_id' => TextColumn::make('user_id')->searchable(),
-            'updated_by' => TextColumn::make('updated_by')->searchable(),
-            'created_by' => TextColumn::make('created_by')->searchable(),
-            'deleted_at' => TextColumn::make('deleted_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
-            'deleted_by' => TextColumn::make('deleted_by')->searchable(),
-            'is_active' => IconColumn::make('is_active')->boolean(),
-        ];
-    }
 }

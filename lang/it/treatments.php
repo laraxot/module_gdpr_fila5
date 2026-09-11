0c8b2c18f2b39f584b6a3096bf5f875584a137a6
Marco Xot <marco76tv@hotmail.com>
2026-09-11 19:06:38 +0200

    .

--- lang/it/treatments.php
+++ lang/it/treatments.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 return [
     'fields' => [
-        'id' => [
-            'label' => 'id',
-        ],
-        'name' => [
-            'label' => 'name',
-        ],
-        'created_at' => [
-            'label' => 'created_at',
-        ],
+        'id' => ['label' => 'id'],
+        'name' => ['label' => 'name'],
+        'created_at' => ['label' => 'created_at'],
+        'active' => ['label' => 'active'],
+        'required' => ['label' => 'required'],
+        'documentVersion' => ['label' => 'documentVersion'],
+        'documentUrl' => ['label' => 'documentUrl'],
+        'weight' => ['label' => 'weight'],
+        'updated_at' => ['label' => 'updated_at'],
     ],
 ];

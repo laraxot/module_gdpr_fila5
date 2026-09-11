a3f1eff8d62d79a92991773bbee3749b6c3575f4
Marco Xot <marco76tv@hotmail.com>
2026-09-11 16:07:13 +0200

    .

--- app/Filament/Clusters/Profile/Resources/ConsentResource/Tables/ConsentsTable.php
+++ app/Filament/Clusters/Profile/Resources/ConsentResource/Tables/ConsentsTable.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Modules\Gdpr\Filament\Clusters\Profile\Resources\ConsentResource\Tables;
 
 use Filament\Tables\Columns\Column;
-use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
@@ -14,16 +13,16 @@ class ConsentsTable extends XotBaseResourceTable
     /**
      * @return array<string, Column>
      */
-    
     public function getTableColumns(): array
     {
         return [
-            'id' => TextColumn::make('id')->numeric()->sortable(),
-            'treatment_id' => TextColumn::make('treatment.name')->sortable(),
-            'subject_id' => TextColumn::make('subject.name')->sortable(),
-            'is_accepted' => IconColumn::make('is_accepted')->boolean(),
-            'data_creazione' => TextColumn::make('data_creazione')->dateTime()->sortable(),
-            'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')->dateTime()->sortable(),
+            'treatment.name' => TextColumn::make('treatment.name')->searchable()->sortable()->wrap(),
+            'subject_id' => TextColumn::make('subject_id')->searchable()->sortable(),
+            'accepted_at' => TextColumn::make('accepted_at')->dateTime()->sortable(),
+            'revoked_at' => TextColumn::make('revoked_at')->dateTime()->sortable(),
+            'id' => TextColumn::make('id')->searchable()->toggleable(isToggledHiddenByDefault: true),
+            'created_at' => TextColumn::make('created_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+            'updated_at' => TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
         ];
     }
 }

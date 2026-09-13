dfb08d095f47c0064d9f1f730fa9aa4410b7f1d5
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-09-13 19:26:32 +0000

    Lint

--- app/Filament/Resources/ProfileResource/Pages/ListProfiles.php
+++ app/Filament/Resources/ProfileResource/Pages/ListProfiles.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\ProfileResource\Pages;
 
-use Filament\Tables;
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Resources\ProfileResource;
 use Modules\User\Filament\Resources\BaseProfileResource\Pages\ListProfiles as UserListProfiles;
 

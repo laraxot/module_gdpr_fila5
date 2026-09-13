36150fa5b2f22e4f50f7b353bbf58cba6751aaf7
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-09-13 14:54:30 +0000

    Lint

--- app/Filament/Clusters/Profile/Resources/ConsentResource/Pages/ListConsents.php
+++ app/Filament/Clusters/Profile/Resources/ConsentResource/Pages/ListConsents.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Clusters\Profile\Resources\ConsentResource\Pages;
 
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Clusters\Profile\Resources\ConsentResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListConsents extends XotBaseListRecords
 {
     protected static string $resource = ConsentResource::class;
-
 }
--- app/Filament/Resources/TreatmentResource/Pages/ListTreatments.php
+++ app/Filament/Resources/TreatmentResource/Pages/ListTreatments.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\TreatmentResource\Pages;
 
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Resources\TreatmentResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListTreatments extends XotBaseListRecords
 {
     protected static string $resource = TreatmentResource::class;
-
-    
 }

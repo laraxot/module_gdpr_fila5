64e47b61a4ce9f357a87504c6207d99c7666bc30
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-09-13 14:54:30 +0000

    Lint

--- app/Filament/Resources/ConsentResource/Pages/ListConsents.php
+++ app/Filament/Resources/ConsentResource/Pages/ListConsents.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\ConsentResource\Pages;
 
-use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Resources\ConsentResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListConsents extends XotBaseListRecords
 {
     protected static string $resource = ConsentResource::class;
-
-    
 }
--- app/Filament/Resources/EventResource/Pages/ListEvents.php
+++ app/Filament/Resources/EventResource/Pages/ListEvents.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\EventResource\Pages;
 
-use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Resources\EventResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListEvents extends XotBaseListRecords
 {
     protected static string $resource = EventResource::class;
-
-    
 }
